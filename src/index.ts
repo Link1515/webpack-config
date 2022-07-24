091c7ad2b93535d14a897b6fb6cf9a100f4626a9
Link <h26281660@gmail.com>
2022-07-24 20:15:37 +0800

    ts with babel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const a = 15
-console.log(a)
+const b = 15
+console.log(b)
+
+const c = 15
+const d = c + 3
 
 function func (a: number, b: number): number {
   return a + b
 }
 
-console.log(func(6, 5))
+console.log(func(c, d))
+
+const a = 15
+
+const p = new Promise((resolve) => {
+  if (a > 15) resolve('OK')
+  else throw new Error('error')
+})
+
+p.then(r => { console.log(r) }).catch(err => { console.log(err) })
--- webpack.config.js
+++ webpack.config.js
@@ -56,16 +56,11 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: 'babel-loader'
       },
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
